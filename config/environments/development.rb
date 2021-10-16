1be8d3694b19c0a652076eb2a210129805b725be
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-10-16 11:36:58 +0100

    devise set up and db migrate called

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # below came from devise install - once in production, change 'localhost' to the actual host.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
